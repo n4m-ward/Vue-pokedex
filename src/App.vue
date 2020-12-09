ff1785793be3945e47bb4a15a9926f7576c5ed03
n4m-ward <65575926+n4m-ward@users.noreply.github.com>
2020-12-09 08:46:21 -0300

    adicionndo algumas funções

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class='column is-half is-offset-one-quarter'>
+      <div v-for="(poke,index) in pokemons" :key='index'>
+        <Pokemon :name='poke.name' :url='poke.url' :num='index +1'/>
+      </div>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Pokemon from './components/Pokemon'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      pokemons: []
+    }
+    
+  },
+  components:{
+    Pokemon
+  },
+  created: function(){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res =>{
+      this.pokemons = res.data.results;
+    })
   }
+
 }
 </script>
 
